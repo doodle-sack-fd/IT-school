2c64ec604a63cfb56efcbfdb4c5f6a4524cc3677
Maxim <titov3000@mail.ru>
2022-11-28 15:49:59 +0300

    minisliders

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,38 @@
 import MainSlider from "./modules/slider/slider-main";
+import MiniSlider from './modules/slider/slider-mini';
 import VideoPlayer from "./modules/playVideo";
 
 window.addEventListener('DOMContentLoaded', () => {
-    const slider = new MainSlider({page: '.page', btns: '.next'});
+    const slider = new MainSlider({container: '.page', btns: '.next'});
     slider.render();
-    const player = new VideoPlayer('.showup .play', '.overlay')
+
+    const miniSliderShowUp = new MiniSlider({
+        container: '.showup__content-slider',
+        prev: '.showup__prev',
+        next: '.showup__next',
+        activeClass: 'card-active',
+        animate: true,
+    });
+    miniSliderShowUp.init();
+
+    const miniModulesSlider = new MiniSlider({
+        container: '.modules__content-slider',
+        prev: '.modules__info-btns .slick-prev',
+        next: '.modules__info-btns .slick-next',
+        activeClass: 'card-active',
+        animate: true,
+        autoPlay: true,
+    });
+    miniModulesSlider.init();
+
+    const miniFeedSlider = new MiniSlider({
+        container: '.feed__slider',
+        prev: '.feed__slider .slick-prev',
+        next: '.feed__slider .slick-next',
+        activeClass: 'feed__item-active',
+    });
+    miniFeedSlider.init();
+
+    const player = new VideoPlayer('.showup .play', '.overlay');
     player.init();
 });
